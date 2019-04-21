d0eeaf370c63680ba46ae397869c0717e96dc3a6
Lisandro Fernandez <kelechul@gmail.com>
2019-04-21 00:08:56 -0300

    Fix total and product_id in items

--- sample.sql
+++ sample.sql
@@ -90,10 +90,10 @@ INSERT INTO purchase_order (id, customer_id, order_date) VALUES (1, 2, '2018-01-
 INSERT INTO purchase_order (id, customer_id, order_date) VALUES (2, 1, '2018-02-13');
 INSERT INTO purchase_order (id, customer_id, order_date) VALUES (3, 2, '2018-02-25');
 
-INSERT INTO item (id, order_id, product_id, quantity, total) VALUES (1, 1, 1, 10, 7.99);
+INSERT INTO item (id, order_id, product_id, quantity, total) VALUES (1, 1, 1, 10, 79.90);
 INSERT INTO item (id, order_id, product_id, quantity, total) VALUES (2, 1, 2, 2, 703.92);
 INSERT INTO item (id, order_id, product_id, quantity, total) VALUES (3, 1, 3, 7, 24.99);
 INSERT INTO item (id, order_id, product_id, quantity, total) VALUES (4, 2, 4, 2, 262.00);
 INSERT INTO item (id, order_id, product_id, quantity, total) VALUES (5, 2, 5, 15, 15000.00);
 INSERT INTO item (id, order_id, product_id, quantity, total) VALUES (6, 3, 1, 7, 55.93);
-INSERT INTO item (id, order_id, product_id, quantity, total) VALUES (7, 3, 5, 18, 18.00);
+INSERT INTO item (id, order_id, product_id, quantity, total) VALUES (7, 3, 6, 18, 18.00);
